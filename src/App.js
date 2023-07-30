8a79a09f4282db0ca14ee90ec456fa60a114c683
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-07-29 22:42:18 -0400

    site update v0.1
    i did stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-
 import { NavBar } from "./components/NavBar";
 import background from "./assets/background.jpg";
+import free from "./assets/free.png";
+import "./App.css";
+import trust from "./assets/quality.png";
+import user from "./assets/friends.png";
+import accurate from "./assets/accuracy.png";
 
 function App() {
   return (
@@ -14,6 +18,51 @@ function App() {
           height: "60vh",
         }}
       ></div>
+
+      <div
+        style={{
+          backgroundColor: "darkblue",
+          marginTop: "-400px",
+          marginLeft: "100px",
+          width: "500px",
+          height: "400px",
+          borderRadius: "60px 60px 0px 0px",
+          paddingTop: "70px",
+          paddingLeft: "50px",
+        }}
+      >
+        <h1 style={{ fontSize: "100px", fontWeight: "bold", color: "#e3f2fd" }}>
+          GNEC <br /> H2O
+        </h1>
+      </div>
+
+      <div
+        style={{
+          backgroundImage: "linear-gradient(darkblue, aqua)",
+          height: "298px",
+        }}
+      ></div>
+
+      <div className="homescreenbox" style={{ marginLeft: "140px" }}>
+        <img src={free} className="homescreenboximage"></img>
+        <h2 className="homescreenboxtext">Free-to-use</h2>
+      </div>
+      <div className="homescreenbox" style={{ marginLeft: "540px" }}>
+        <img src={trust} className="homescreenboximage"></img>
+        <h2 className="homescreenboxtext">Trustworthy</h2>
+      </div>
+      <div className="homescreenbox" style={{ marginLeft: "940px" }}>
+        <img src={user} className="homescreenboximage"></img>
+        <h2 className="homescreenboxtext" style={{ marginLeft: "-5px" }}>
+          User-friendly
+        </h2>
+      </div>
+      <div className="homescreenbox" style={{ marginLeft: "1340px" }}>
+        <img src={accurate} className="homescreenboximage"></img>
+        <h2 className="homescreenboxtext" style={{ marginLeft: "15px" }}>
+          Accurate
+        </h2>
+      </div>
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,12 @@ import logo from "../assets/logo.svg";
 export const NavBar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#0">
+      <nav
+        className="navbar navbar-expand-lg bg-body-tertiary"
+        style={{ background: "#296bd6" }}
+      >
+        <div className="container-fluid" style={{ paddingLeft: "130px" }}>
+          <a className="navbar-brand" href="#">
             <img src={logo} alt="logo" width="60" height="48" />
           </a>
           <button
@@ -22,19 +25,19 @@ export const NavBar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#0">
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#0">
+                <a className="nav-link" href="#">
                   Link
                 </a>
               </li>
               <li className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle"
-                  href="#0"
+                  href="#"
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
@@ -43,12 +46,12 @@ export const NavBar = () => {
                 </a>
                 <ul className="dropdown-menu">
                   <li>
-                    <a className="dropdown-item" href="#0">
+                    <a className="dropdown-item" href="#">
                       Action
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#0">
+                    <a className="dropdown-item" href="#">
                       Another action
                     </a>
                   </li>
@@ -56,27 +59,16 @@ export const NavBar = () => {
                     <hr className="dropdown-divider"></hr>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#0">
+                    <a className="dropdown-item" href="#">
                       Something else here
                     </a>
                   </li>
                 </ul>
               </li>
               <li className="nav-item">
-                <a href="#0" className="nav-link disabled">Disabled</a>
+                <a className="nav-link disabled">Disabled</a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              ></input>
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
