7438f6050e37f836200eb5d158f98047ee032810
andrewisaboomer <dzeng49@gmail.com>
2023-07-30 21:27:23 -0400

    try again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import NavBar from "./components/NavBar";
 import background from "./assets/background.jpg";
-// import free from "./assets/free.png";
+import free from "./assets/free.png";
 import "./App.css";
 import trust from "./assets/quality.png";
 import user from "./assets/friends.png";
 import accurate from "./assets/accuracy.png";
-
-const free = "./assets/free.png"
 
 function Homebox(name, image) {
   return (
