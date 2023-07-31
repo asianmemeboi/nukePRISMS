d2248a56ec6e3ae3e58dc734adebbc1d93648bec
andrewisaboomer <dzeng49@gmail.com>
2023-07-30 23:18:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
 
-app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+App.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
 
-app.get("/*", function(req, res) {
+App.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
