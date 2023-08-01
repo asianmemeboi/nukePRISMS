6bcbde85b0a408a9a8bf759dee238d01375267e2
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-07-31 23:54:30 -0400

    fixed import issues with css file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import "./App.css";
+import "../App.css";
 import {Link, Outlet} from "react-router-dom";
 
 function NavigationBar() {
