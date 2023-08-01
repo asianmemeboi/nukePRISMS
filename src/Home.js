4bab690d2f3bdab827670deae00cd963dcbb1dfc
andrewisaboomer <dzeng49@gmail.com>
2023-08-01 18:03:33 -0400

    try parallax

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,8 @@ import React from 'react';
 import trust from "./assets/quality.png";
 import user from "./assets/friends.png";
 import accurate from "./assets/accuracy.png";
+import ScrollContainer from "./Parallax/ScrollContainer";
+import ParallaxElement from "./Parallax/ParllaxElement"; 
 
 class HomeBox extends React.Component {
   constructor(props) {
@@ -50,61 +52,6 @@ class HomeBox extends React.Component {
   }
 }
 
-// var Component = React.createClass({
-//   getInitialState: function() {
-//       return {
-//           style: {
-//               logoHeight: 200
-//           }
-//       }
-//   },
-
-//   componentDidMount: function() {
-//       window.addEventListener('scroll', this.handleScroll);
-//   },
-
-//   componentWillUnmount: function() {
-//       window.removeEventListener('scroll', this.handleScroll);
-//   },
-
-//   handleScroll: function(event) {
-//       let scrollTop = window.scrollY,
-//               minHeight = 30,
-//               logoHeight = Math.max(minHeight, 200 - scrollTop);
-//       this.setState({
-//           style: {
-//               logoHeight: logoHeight
-//           }
-//       });
-//   },
-
-//   render: function() {
-//       return (
-//           <div>
-//               <div className="container" style={{height: this.state.style.logoHeight}}>
-//                   <img src='https://www.import.io/wp-content/uploads/2017/10/React-logo-1.png' className='logo'/>
-//               </div>
-//               <div className="empty-space">
-
-//               </div>
-//           </div>
-//       );
-//   }
-// });
-
-// function Homebox(name, image, imageMargin) {
-//   return (
-//     <div
-//     className="homescreenbox"
-//     id="scrollingdiv"
-//     style={{ marginLeft: imageMargin }}
-//   >
-//       <img src={image} alt={name} className="homescreenboximage"></img>
-//       <h2 className="homescreenboxtext">{name}</h2>
-//     </div>
-//   )
-// }
-
 function App() {
   return (
     <>
@@ -142,40 +89,17 @@ function App() {
           height: "298px",
         }}
       ></div>
-
       
-      {/* { Homebox("Free-to-use", free, "140px") }
-      { Homebox("Trustworthy", trust, "540px") }
-      { Homebox("User-friendly", user, "940px") }
-      { Homebox("Accurate", accurate, "1340px") } */}
-      
-      <HomeBox name="Free-to-use" image={free} imageMargin="140px" />
-      <HomeBox name="Trustworthy" image={trust} imageMargin="540px" />
-      <HomeBox name="User-friendly" image={user} imageMargin="940px" />
-      <HomeBox name="Accurate" image={accurate} imageMargin="1340px" />
+      <ParallaxElement dataPercent="20">
+        <HomeBox name="Free-to-use" image={free} imageMargin="140px" />
+        <HomeBox name="Trustworthy" image={trust} imageMargin="540px" />
+        <HomeBox name="User-friendly" image={user} imageMargin="940px" />
+        <HomeBox name="Accurate" image={accurate} imageMargin="1340px" />
+      </ParallaxElement>
 
       <div style={{ backgroundColor: "aqua", height: "1000px" }}></div>
     </>
   );
 }
-
-// function $(x) {
-//   return document.getElementById(x);
-// }
-
-// $(window).scroll(function () {
-//   var scrollTop = $(window).scrollTop();
-//   var maxHeight = -1;
-//   if (scrollTop < 200) {
-//       maxHeight = 150;
-//   } else if (scrollTop > 400) {
-//       maxHeight = 75;
-//   } else {
-//       maxHeight = 150 - 75 * ((scrollTop - 200)) / 200;
-//   }
-//   $('#scrollingdiv').css({
-//       'margin-top': -maxHeight + "px"
-//   });
-// })
 
 export default App;
