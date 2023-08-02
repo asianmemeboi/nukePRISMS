0ded632a759b77a48f0dfd2446b80df419e09b9b
andrewisaboomer <dzeng49@gmail.com>
2023-08-01 21:54:18 -0400

    make it work

--- src/components/ParallaxElement.js
+++ src/components/ParallaxElement.js
@@ -1,10 +1,10 @@
-import { useEffect, useContext } from "react";
-import { ScrollContext } from "./ScrollContainer";
+// import { useEffect, useContext } from "react";
+// import { ScrollContext } from "./ScrollContainer";
 
-var props = function() {
-    const context = useContext(ScrollContext);
-    useEffect(() => context.addBlock(props), []);
-    return props.children;
-  };
+// var props = function() {
+//     const context = useContext(ScrollContext);
+//     useEffect(() => context.addBlock(props), []);
+//     return props.children;
+//   };
 
-export default props;
+// export default props;
