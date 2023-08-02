5cdeb72e7d7bc7f89161926112c21d4bd536d22b
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-08-02 10:28:05 -0400

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,10 @@
 function Login() {
     return (
         <>
-        <div style={{backgroundImage: "linear-gradient(darkblue, aqua)", height: "970px"}}>
+        <div style={{backgroundImage: "linear-gradient(darkblue, aqua)", backgroundSize:"cover"}}>
             
         </div>
         <div style={{height: "90px", width: "90px", marginLeft: "500px", marginTop: "-200px", backgroundColor: "#ffffff"}}>
-            <h1> hi </h1>
         </div>
         </>
     )
