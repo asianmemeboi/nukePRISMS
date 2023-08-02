e55b5811077819ff502f905b687ca42bc3295276
andrewisaboomer <dzeng49@gmail.com>
2023-08-01 23:21:20 -0400

    undo train

--- src/Home.js
+++ src/Home.js
@@ -7,13 +7,12 @@ import user from "./assets/friends.png";
 import accurate from "./assets/accuracy.png";
 // import ScrollContainer from "./components/ScrollContainer";
 // import ParallaxElement from "./components/ParallaxElement"; 
-import LocomotiveScroll from 'locomotive-scroll';
-
-// eslint-disable-next-line no-unused-vars
-const scroll = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
-  smooth: true
-});
+// import LocomotiveScroll from 'locomotive-scroll';
+// // eslint-disable-next-line no-unused-vars
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector('[data-scroll-container]'),
+//   smooth: true
+// });
 
 class HomeBox extends React.Component {
   constructor(props) {
