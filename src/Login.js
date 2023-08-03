ff6143027c66fc3bec9c501269a1e7eaed49a7c8
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-08-03 14:33:10 -0400

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,14 @@
 import Form from "react-bootstrap/Form";
 function Login() {
-    return (
-        <>
-        <div
+  return (
+    <>
+      <div
         style={{
           backgroundImage: `linear-gradient(darkblue, aqua)`,
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
           backgroundAttachment: "fixed",
-          height: "95vh"
+          height: "95vh",
         }}
       ></div>
 
@@ -20,28 +20,35 @@ function Login() {
           width: "60vh",
           height: "70vh",
           paddingTop: "30px",
+          paddingLeft: "20px",
+          paddingRight: "20px",
         }}
-      > <h1 style={{textAlign: "center"}}> Login </h1>
-      <Form>
-        <Form.Group>
-          <Form.Label htmlFor="username">Username</Form.Label>
-          <Form.Control type="text" placeholder="Type your username"></Form.Control>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label htmlFor="password">Password</Form.Label>
-          <Form.Control type="password" placeholder="Type your password" aria-describedby="passwordHelp"></Form.Control>
-          <Form.Text id="passwordHelp" muted>Your password must be 8-20 characters long.</Form.Text>
-        </Form.Group>
-      
-
-      </Form>
-        
+      >
+        {" "}
+        <h1 style={{ textAlign: "center" }}> Login </h1>
+        <Form>
+          <Form.Group>
+            <Form.Label htmlFor="username">Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Type your username"
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="password">Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Type your password"
+              aria-describedby="passwordHelp"
+            ></Form.Control>
+            <Form.Text id="passwordHelp" muted>
+              Your password must be 8-20 characters long.
+            </Form.Text>
+          </Form.Group>
+        </Form>
       </div>
-
-      
-        </>
-    )
+    </>
+  );
 }
-
 
 export default Login;
