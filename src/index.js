868a5595508679a3aab6c414df3ec12cd1c5657c
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-08-03 15:45:19 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
-import Home from './Home';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import NavigationBar from './components/NavBar';
+import NavigationBar from "./components/NavBar";
 import ErrorPage from "./error-page";
 import Login from "./Login";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    
     path: "/",
     element: <NavigationBar />,
     errorElement: <ErrorPage />,
     children: [
-    {
-      path: "home",
-      element: <Home />,
-    },
-    {
-      path:"login",
-      element: <Login />,
-    }
-    ]
-}]);
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} /> 
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
