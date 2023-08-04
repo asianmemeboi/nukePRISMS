79a4ad047a3e2ebb616111430fc1b46726589bb5
andrewisaboomer <dzeng49@gmail.com>
2023-08-04 18:35:36 -0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -53,16 +53,18 @@ class ProcessSection extends React.Component {
 
   render() {
     return (
-      <div data-scroll data-scroll-sticky data-scroll-target={`#${this.state.header}`} style={{position: "sticky"}}>
-        <div id={this.state.header} style={{position: "relative", top: "-15vh", height: "100vh", float: "left"}}></div>
-        <div
-        className="process-section-text">
-          <h1>{this.state.header}</h1>
-          <p>{this.state.description}</p>
-        </div>
-        <div
-        className="process-section-image-wrapper">
-          <img className="process-section-image" src={this.state.image} alt={this.state.header}></img>
+      <div data-scroll data-scroll-speed="1">
+        <div data-scroll data-scroll-sticky data-scroll-target={`#${this.state.header}`} style={{position: "sticky"}}>
+          <div id={this.state.header} style={{position: "relative", top: "-15vh", height: "100vh", float: "left"}}></div>
+          <div
+          className="process-section-text">
+            <h1>{this.state.header}</h1>
+            <p>{this.state.description}</p>
+          </div>
+          <div
+          className="process-section-image-wrapper">
+            <img className="process-section-image" src={this.state.image} alt={this.state.header}></img>
+          </div>
         </div>
       </div>
     );
