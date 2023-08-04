fe24422acd7a1dfd279149d48b27fa0085035eab
andrewisaboomer <dzeng49@gmail.com>
2023-08-04 11:23:30 -0400

    add water

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import free from "./assets/free.png";
 import trust from "./assets/quality.png";
 import user from "./assets/friends.png";
 import accurate from "./assets/accuracy.png";
+import water from "./assets/water.png";
 import "./App.css";
 import React, { useEffect } from 'react';
 import LocomotiveScroll from 'locomotive-scroll';
@@ -114,7 +115,7 @@ function App() {
           }}
         ></div>
 
-        <div data-scroll data-scroll-speed="6" data-scroll-sticky data-scroll-target="#something" style={{position: "sticky", zIndex: "100"}}>
+        <div data-scroll data-scroll-speed="6" data-scroll-sticky data-scroll-target="#something" style={{position: "sticky", zIndex: "1"}}>
           <HomeBox name="Free-to-use" image={free} imageMargin="140px" />
           <HomeBox name="Trustworthy" image={trust} imageMargin="540px" />
           <HomeBox name="User-friendly" image={user} imageMargin="940px" />
@@ -124,6 +125,27 @@ function App() {
         <div data-scroll data-scroll-speed="3" style={{ marginTop: "-211px", backgroundColor: "aqua", height: "1500px", zIndex: "-100" }}></div>
 
         <div id="something" style={{position: "absolute", top: "1100px", height: "1000px"}}></div>
+
+        <div data-scroll data-scroll-speed="3"
+          style={{
+            backgroundImage: `url(${water})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            height: "60vh",
+            position:"absolute",
+            top:"1700px"
+          }}
+        ></div>
+
+        <div data-scroll data-scroll-speed="3"
+          style={{
+            position:"absolute",
+            top:"2200px",
+            backgroundColor: "#289fdc",
+            height: "400px",
+            zIndex: "-100"
+          }}
+        ></div>
       </div>
     </div>
   );
