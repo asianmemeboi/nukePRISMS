30faf253fe6f89a7f85981cf3e941b17c85fbb26
andrewisaboomer <dzeng49@gmail.com>
2023-08-04 17:10:48 -0400

    vdas

--- src/Home.js
+++ src/Home.js
@@ -53,14 +53,14 @@ class ProcessSection extends React.Component {
 
   render() {
     return (
-      <div>
+      <div data-scroll data-scroll-sticky data-scroll-target="#test">
         <div id={this.state.header} style={{position: "relative", top: "-200px", height: "50vh", float: "left"}}></div>
-        <div data-scroll data-scroll-sticky data-scroll-target="#test"
+        <div
         className="process-section-text">
           <h1>{this.state.header}</h1>
           <p>{this.state.description}</p>
         </div>
-        <div data-scroll data-scroll-sticky data-scroll-target={`#${this.state.header}`} 
+        <div /* data-scroll-sticky data-scroll-target={`#${this.state.header}`} */
         className="process-section-image-wrapper">
           <img className="process-section-image" src={this.state.image} alt={this.state.header}></img>
         </div>
