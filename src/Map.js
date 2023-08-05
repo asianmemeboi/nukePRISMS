9d7d1ea224c3f78ced4624f5037fe94436631d7e
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-08-05 11:33:03 -0400

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -3,11 +3,17 @@ import { useMemo } from "react";
 import "./App.css";
 
 const Map = () => {
+  const myLatLng = { lat: 40.247132, lng: -74.838578 };
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyBnTLQEP9URuJROG3DnJrqvYaerBeX3pDw",
   });
   const center = useMemo(() => ({ lat: 40.23287, lng: -74.856071 }), []);
 
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "First Water Testing Point"
+  })
   return (
     <div className="Themap">
       {!isLoaded ? (
