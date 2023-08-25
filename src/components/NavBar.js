23eec66f0f9f70b40ff3913247f468f6c819be34
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-08-25 10:00:26 -0400

    bruh

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import logo from "../assets/logo.svg";
+import logo from "../assets/logo.png";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
