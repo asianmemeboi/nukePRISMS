bf1117c38b844d0094f515850bf18a63c01b43be
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-08-25 13:16:53 -0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -14,16 +14,31 @@ function App() {
             // backgroundSize: "cover",
             // backgroundRepeat: "no-repeat",
             height: "100vh",
-            paddingTop: "30vh"
+            paddingTop: "25vh",
           }}
-        ><h1
-        style={{ fontSize: "600%", fontWeight: "bold", color: "#e3f2fd", textAlign:"center"}}
-      >
-        nukePRISMS: 
-      </h1>
-      <h2 style={{ fontSize: "250%", color: "#e3f2fd", paddingTop: "5vh", textAlign:"center"}}> Made by Students, For Students</h2>
-      </div>
-      
+        >
+          <h1
+            style={{
+              fontSize: "600%",
+              fontWeight: "bold",
+              color: "#e3f2fd",
+              textAlign: "center",
+            }}
+          >
+            nukePRISMS:
+          </h1>
+          <h2
+            style={{
+              fontSize: "250%",
+              color: "#e3f2fd",
+              paddingTop: "2vh",
+              textAlign: "center",
+            }}
+          >
+            {" "}
+            Made by Students, For Students
+          </h2>
+        </div>
       </div>
     </div>
   );
