b8ff761ecb2f6702293224fd9476add66ea29c57
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-08-25 14:13:56 -0400

    attempt at centering stuff in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,13 +22,18 @@ function NavigationBar() {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav
-              className="me-auto"
-              style={{ paddingLeft: "-50px", verticalAlign: "center" }}
-            >
-              <Nav.Link href={`/#/`}>Home</Nav.Link>
-              <Nav.Link href={`/#/login/`}>Login</Nav.Link>
-              <NavDropdown title="Water" id="basic-nav-dropdown">
+            <Nav className="me-auto">
+              <Nav.Link href={`/#/`} class="nav-text">
+                Home
+              </Nav.Link>
+              <Nav.Link href={`/#/login/`} class="nav-text">
+                Login
+              </Nav.Link>
+              <NavDropdown
+                title="Water"
+                id="basic-nav-dropdown"
+                class="nav-text"
+              >
                 <NavDropdown.Item href={`/#/submit/`}>
                   Submit a sample
                 </NavDropdown.Item>
