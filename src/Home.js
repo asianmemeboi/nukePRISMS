7704210d1cc23ea751dbfbd9ccdbb399f1830768
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-08-24 20:49:08 -0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -13,30 +13,14 @@ function App() {
             backgroundImage: `url(${background})`,
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
-            height: "60vh",
+            height: "100vh",
           }}
         ></div>
-
-        <div
-          data-scroll
-          data-scroll-speed="3"
-          style={{
-            backgroundColor: "darkblue",
-            marginTop: "-400px",
-            marginLeft: "100px",
-            width: "500px",
-            height: "390px",
-            borderRadius: "60px 60px 0px 0px",
-            paddingTop: "70px",
-            paddingLeft: "50px",
-          }}
-        >
           <h1
             style={{ fontSize: "100px", fontWeight: "bold", color: "#e3f2fd" }}
           >
-            GNEC <br /> H2O
+            GNEC H2O
           </h1>
-        </div>
       </div>
     </div>
   );
