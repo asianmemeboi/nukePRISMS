b52262381d3ebf25cbce831fcdde7c0b544d460b
andrewisaboomer <dzeng49@gmail.com>
2023-08-24 21:39:04 -0400

    sadfsda

--- src/Home.js
+++ src/Home.js
@@ -6,23 +6,24 @@ import React from "react";
 function App() {
   return (
     <div>
+      <CoolBackground />
       <div style={{ height: "200vh" }}>
         <div
           style={{
-            backgroundImage: `url(${background})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
+            // backgroundImage: `url(${background})`,
+            // backgroundSize: "cover",
+            // backgroundRepeat: "no-repeat",
             height: "100vh",
             paddingTop: "30vh"
           }}
         ><h1
-        style={{ fontSize: "500%", fontWeight: "bold", color: "#e3f2fd", }}
+        style={{ fontSize: "600%", fontWeight: "bold", color: "#e3f2fd", textAlign:"center"}}
       >
         nukePRISMS: 
       </h1>
-      <h2 style={{ fontSize: "400%", fontWeight: "bold", color: "#e3f2fd", paddingTop: "10vh"}}> Made by Students, For Students</h2>
+      <h2 style={{ fontSize: "250%", color: "#e3f2fd", paddingTop: "5vh", textAlign:"center"}}> Made by Students, For Students</h2>
       </div>
-      <CoolBackground />
+      
       </div>
     </div>
   );
--- src/components/CoolBackground.js
+++ src/components/CoolBackground.js
@@ -9,24 +9,10 @@ class Application extends React.Component {
         this.state = {
             animationClass: 'animating'
         }
-        // this.changeState = this.changeState.bind(this);
     }
 
-    // changeState(){
-    //     if (this.state.animationClass === 'animating'){
-    //         this.setState({
-    //             animationClass: 'animating paused'
-    //         });
-    //     } else {
-    //         this.setState({
-    //             animationClass: 'animating'
-    //         });
-    //     }
-    // }
     render() {
         return <div className={this.state.animationClass}>
-        {/* <h1>Pure CSS3 Animated Gradient Background</h1>
-        <button onClick={this.changeState}>Stop / Start</button> */}
         </div>;
     }
 }
