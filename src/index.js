55e51ccea2ee6f35ed5d4cb337b0ae7a94fc18cf
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-08-25 23:25:26 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -44,10 +44,12 @@ const router = createHashRouter([
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+document.body.style.overflow = "hidden";
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
