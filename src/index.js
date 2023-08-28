4c7133ca34902c2295606e5192cbe308dfbb0d10
andrewisaboomer <dzeng49@gmail.com>
2023-08-28 15:28:43 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import NavigationBar from "./components/NavBar";
 import ErrorPage from "./error-page";
+import Classes from "./Classes.js"
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const router = createHashRouter([
