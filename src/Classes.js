05e97f61d4a0046741efee338dbad942ac209587
andrewisaboomer <dzeng49@gmail.com>
2023-08-29 23:11:55 -0400

    Update Classes.js

--- src/Classes.js
+++ src/Classes.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 class Class extends React.Component {
   constructor(props) {
@@ -18,35 +18,75 @@ class Class extends React.Component {
   }
 }
 
+const dummyDatabase = {
+  "AP Biology" : {
+    name: "AP Biology",
+    periods : {
+      6 : "Joeseph Bay",
+      6 : "Joeseph Woska",
+      7 : "Random Guy"
+    },
+    subject: "Biology",
+    field: "STEM"
+  },
+  "AP Calculus BC" : {
+    name: "AP Calculus BC",
+    periods : {
+      6 : "Kenneth Jones",
+      8 : "Andrew Bleckner"
+    },
+    subject: "Mathematics",
+    field: "STEM"
+  }
+};
+
 function Classes() {
-  useEffect(
-    () => {
-      const script = document.createElement("script");
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState({
+    query: '',
+    list: []
+  });
 
-      const scriptText = document.createTextNode(`
-      function searchClasses() { 
-        console.log('hi'); 
-      }
-      `);
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+    const r = Object.fromEntries(Object.entries(dummyDatabase).filter(([k,v]) => (k.name.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1)));
+    setResults({
+      query: e.target.value,
+      list: r
+    });
+  };
 
-      script.appendChild(scriptText);
-      document.head.appendChild(script);
+  // useEffect(
+  //   () => {
+  //     const script = document.createElement("script");
 
-      return () => {
-        document.head.removeChild(script);
-      }
-    }, []
-  );
+  //     const scriptText = document.createTextNode(`
+  //     function searchClasses() { 
+  //       console.log('hi'); 
+  //     }
+  //     `);
+
+  //     script.appendChild(scriptText);
+  //     document.head.appendChild(script);
+
+  //     return () => {
+  //       document.head.removeChild(script);
+  //     }
+  //   }, []
+  // );
   
   return (
     <div>
       <h3>Classes here</h3>
-      <input type="text" onKeyUp="searchClasses()" />
-      <Class subject="AP Bio"/>
+      <input type="search" value={query} onChange={handleChange} />
+      {(results.query === '' ? "" : state.list.values().map(post => {
+            return <li key={post.name}>{post.name}</li>
+          }))}
+      {/* <Class subject="AP Bio"/>
       <Class subject="Honors bio"/>
       <Class subject="AP Calc"/>
       <Class subject="AP Bio"/>
-      <Class subject="Honors bio"/>
+      <Class subject="Honors bio"/> */}
     </div>
   );
 }
