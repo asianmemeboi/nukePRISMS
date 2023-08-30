7497e359c41f550e29c77aafb50122a133be8112
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-08-30 09:39:03 -0400

    Update Classes.js

--- src/Classes.js
+++ src/Classes.js
@@ -89,9 +89,9 @@ function Classes() {
   
   return (
     <div>
-      <div style={{height: "30vh", backgroundColor: "darkaqua", paddingTop: "10vh"}}>
-        <h3 style={{left: "50%", color: "white"}}>Classes</h3>
-        <input type="search" onChange={handleChange} style={{left: "50%"}}/>
+      <div style={{height: "30vh", backgroundColor: "#032896", paddingTop: "10vh"}}>
+        <h3 style={{left: "50%", textAlign: "center"}}>Classes</h3>
+        <input type="search" onChange={handleChange} style={{left: "50%", position: "absolute"}}/>
       </div>
       
       
