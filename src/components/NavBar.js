e60ff48b1a74485c305962acd420d90fe39161c7
Kingasian <58711340+asianmemeboi@users.noreply.github.com>
2023-09-07 09:08:13 -0400

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,85 +4,90 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "../App.css";
 import { Outlet } from "react-router-dom";
-import {useGoogleLogin, googleLogout} from '@react-oauth/google'
-import React, {useState,useEffect} from 'react';
-import axios from 'axios';
+import { useGoogleLogin, googleLogout } from "@react-oauth/google";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function NavigationBar() {
   const [user, setUser] = useState([]);
-  const [ profile, setProfile ] = useState([]);
+  const [profile, setProfile] = useState([]);
   const login = useGoogleLogin({
-      onSuccess: (codeResponse) => { setUser(codeResponse); console.log(codeResponse); },
-      onError: (error) => console.log('Login Failed:', error)
+    onSuccess: (codeResponse) => {
+      setUser(codeResponse);
+      console.log(codeResponse);
+    },
+    onError: (error) => console.log("Login Failed:", error),
   });
 
   function refreshPage() {
     window.location.reload(false);
   }
-  
+
+  useEffect(() => {
+    setProfile(null);
+    window.userdata = {
+      user: null,
+      profile: null,
+    };
+    if (sessionStorage.getItem("login")) {
+      const sessionUser = JSON.parse(sessionStorage.getItem("login"));
+      if (sessionUser) {
+        axios
+          .get(
+            `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${sessionUser.access_token}`,
+            {
+              headers: {
+                Authorization: `Bearer ${user.access_token}`,
+                Accept: "application/json",
+              },
+            }
+          )
+          .then((res) => {
+            setProfile(res.data);
+          })
+          .catch((err) => console.log(err));
+        window.userdata = {
+          user: sessionUser,
+          profile: profile,
+        };
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   useEffect(
     () => {
-      setProfile(null);
-      window.userdata = {
-        user: null,
-        profile: null
+      if (user) {
+        sessionStorage.setItem("login", JSON.stringify(user));
+        axios
+          .get(
+            `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+            {
+              headers: {
+                Authorization: `Bearer ${user.access_token}`,
+                Accept: "application/json",
+              },
+            }
+          )
+          .then((res) => {
+            setProfile(res.data);
+          })
+          .catch((err) => console.log(err));
+        window.userdata = {
+          user: user,
+          profile: profile,
+        };
       }
-      if (sessionStorage.getItem("login")) {
-        const sessionUser = JSON.parse(sessionStorage.getItem("login"));
-        if (sessionUser) {
-            axios
-                .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${sessionUser.access_token}`, {
-                    headers: {
-                        Authorization: `Bearer ${user.access_token}`,
-                        Accept: 'application/json'
-                    }
-                })
-                .then((res) => {
-                    setProfile(res.data);
-                })
-                .catch((err) => console.log(err));
-          window.userdata = {
-            user: sessionUser,
-            profile: profile
-          };
-        }
-      }
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []
+    [user]
   );
 
-
-  useEffect(
-      () => {
-          if (user) {
-            sessionStorage.setItem("login", JSON.stringify(user));
-              axios
-                  .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                      headers: {
-                          Authorization: `Bearer ${user.access_token}`,
-                          Accept: 'application/json'
-                      }
-                  })
-                  .then((res) => {
-                      setProfile(res.data);
-                  })
-                  .catch((err) => console.log(err));
-            window.userdata = {
-              user: user,
-              profile: profile
-            };
-            refreshPage();
-          }
-      },
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      [ user ]
-  );
-  
   // log out function to log the user out of google and set the profile array to null
   const logOut = () => {
-      sessionStorage.removeItem("login");
-      googleLogout();
-      setProfile(null);
+    sessionStorage.removeItem("login");
+    googleLogout();
+    setProfile(null);
   };
   return (
     <>
@@ -110,33 +115,38 @@ function NavigationBar() {
                 Home
               </Nav.Link>
               {profile ? (
-                <Nav.Link href={`/#/classes`} style={{color:"white"}}> Classes </Nav.Link>
+                <Nav.Link href={`/#/classes`} style={{ color: "white" }}>
+                  {" "}
+                  Classes{" "}
+                </Nav.Link>
               ) : (
                 <></>
               )}
-              
             </Nav>
             <Nav>
               {profile ? (
                 <div>
                   <Nav className="justify-content-end">
-                  <Navbar.Brand href={`/#/`}>
-                    <img
-                      alt="profilepicture"
-                      src={profile.picture}
-                      width="30"
-                      height="30"
-                      className="d-inline-block align-top"
-                    />{" "}
-                    User: {profile.name}
-                  </Navbar.Brand>
-                  
-                    <Nav.Link onClick={logOut} style={{ color: "white" }}>Log Out</Nav.Link>
+                    <Navbar.Brand href={`/#/`}>
+                      <img
+                        alt="profilepicture"
+                        src={profile.picture}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                      />{" "}
+                      User: {profile.name}
+                    </Navbar.Brand>
+
+                    <Nav.Link onClick={logOut} style={{ color: "white" }}>
+                      Log Out
+                    </Nav.Link>
                   </Nav>
-                  
                 </div>
               ) : (
-                <Nav.Link onClick={() => login()} style={{ color: "white" }}>Sign in </Nav.Link>
+                <Nav.Link onClick={() => login()} style={{ color: "white" }}>
+                  Sign in{" "}
+                </Nav.Link>
               )}
             </Nav>
           </Navbar.Collapse>
